4558aefcf214fb13a6ec2bf88ea190fe123e6e69
Yogesh Meena <168392069+yogeshthedev@users.noreply.github.com>
2025-06-29 19:53:00 +0530

    commit

---
+++ small-js-projects/form-validator/app.js
@@ -0,0 +1,8 @@
+const form = document.querySelector("form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+
+form.addEventListener("submit", function (e) {
+
+});
