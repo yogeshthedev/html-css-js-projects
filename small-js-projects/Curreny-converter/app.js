2ec9de5dbf8967ded1b1e17a5341c634fb9cbc3b
Yogesh Meena <168392069+yogeshthedev@users.noreply.github.com>
2025-07-01 13:21:23 +0530

    commit

--- small-js-projects/Curreny-converter/app.js
+++ small-js-projects/Curreny-converter/app.js
@@ -1,11 +1,24 @@
 const rates = {
-    USD: 1,
-    EUR: 0.85,
-    INR: 75,
-    GBP: 0.75,
-    JPY: 110
-  };
-  
+  USD: 1,
+  EUR: 0.85,
+  INR: 75,
+  GBP: 0.75,
+  JPY: 110,
+};
 
-let amount = document.querySelector('#amount');
+let amount = document.querySelector("#amount");
+let fromCurrency = document.getElementById("from-currency");
+let toCurrency = document.getElementById("to-currency");
+let converBtn = document.querySelector("#convert");
+let result = document.querySelector("#result");
 
+converBtn.addEventListener("click", () => {
+  if (amount.value == "") {
+    result.innerText = "Please enter amount";
+  } else if (amount.value <= 0) {
+    result.innerText = "Write Valid Amount";
+    let convertedAmount =
+      (amount.value / rates[fromCurrency.value]) * rates[toCurrency.value];
+    result.innerText = convertedAmount.toFixed(2);
+  }
+});
